e3a2efa91b07b36e34635358f84081af226e2b01
Madrigal1018 <107096229+Madrigal1018@users.noreply.github.com>
2023-08-23 01:20:51 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "MadrigalAguirre"
+  location = "eastus"
+}
+
+
+resource "azurerm_data_factory" "df" {
+  name     = "DataFactoryMadrigalAguirre1018"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+}
