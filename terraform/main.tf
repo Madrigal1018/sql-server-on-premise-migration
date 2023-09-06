aa36fc08c901f310615ee89c7053e2d5e23248e4
zebek95 <saguirrer.dev@gmail.com>
2023-09-06 11:10:57 -0500

    Add: Add data factory repository
    Adding data factory code to github repository

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,5 +22,17 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_data_factory" "df" {
   name                = "DataFactoryMadrigalAguirre1018"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location = azurerm_resource_group.rg.location
+
+  github_configuration {
+    account_name = "zebek95"
+    branch_name = "main"
+    git_url = "https://github.com"
+    repository_name = "sql-server-on-premise-migration"
+    root_folder = "data_factory"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
